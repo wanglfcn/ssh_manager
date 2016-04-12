917c5b0c9da111765e60549adbd98824819f22a4
wanglifeng <wanglifeng@yidian-inc.com>
2016-04-12 21:59:51 +0800

    fix bug, now list can be see in screen

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <ncurses.h>
 #include <menu.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "Node.h"
+
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
 
 int main()
 {
@@ -22,6 +26,7 @@ int main()
 	noecho();
 	keypad(stdscr, TRUE);
 	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_CYAN, COLOR_BLACK);
 
 	while(node)
 	{
@@ -37,7 +42,7 @@ int main()
 		node_items[i] = new_item(node->item_name, node->ip);
 		node = node->next;
 	}
-	node_items[i] = (ITEM *)NULL;
+	node_items[i] = NULL;
 
 	menu = new_menu(node_items);
 	window = newwin(10, 40, 4, 4);
@@ -46,12 +51,18 @@ int main()
 
 	set_menu_win(menu, window);
 	set_menu_sub(menu, derwin(window, 6, 38, 3, 1));
+	set_menu_format(menu, 5, 1);
 	set_menu_mark(menu, ">");
 
 	box(window, 0, 0);
+	print_in_middle(window, 1, 0, 40, "My Menu", COLOR_PAIR(1));
 	mvwaddch(window, 2, 0, ACS_LTEE);
 	mvwhline(window, 2, 1, ACS_HLINE, 38);
 	mvwaddch(window, 2, 39, ACS_RTEE);
+
+	post_menu(menu);
+	wrefresh(window);
+
 	mvprintw(LINES - 2, 0, "F1 to exit");
 	refresh();
 
@@ -79,4 +90,28 @@ int main()
 		free_item(node_items[i]);
 	}
 	endwin();
+	return 0;
+}
+
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color)
+{	int length, x, y;
+	float temp;
+
+	if(win == NULL)
+		win = stdscr;
+	getyx(win, y, x);
+	if(startx != 0)
+		x = startx;
+	if(starty != 0)
+		y = starty;
+	if(width == 0)
+		width = 80;
+
+	length = strlen(string);
+	temp = (width - length)/ 2;
+	x = startx + (int)temp;
+	wattron(win, color);
+	mvwprintw(win, y, x, "%s", string);
+	wattroff(win, color);
+	refresh();
 }
